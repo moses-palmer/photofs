2bd4e52fa0d85ddeea78a03908c3b03da1321e77
Moses Palmér <mosespalmer@gmail.com>
2014-11-10 21:10:55 +0100

    Define command line arguments explicitly

--- lib/photofs/__main__.py
+++ lib/photofs/__main__.py
@@ -23,38 +23,44 @@ def main():
         help = 'Run the daemon in the foreground.',
         action = 'store_true')
 
+    parser.add_argument('--photo-path',
+        help = 'The name of the top level directory that contains photos.')
+
+    parser.add_argument('--video-path',
+        help = 'The name of the top level directory that contains videos.')
+
+    parser.add_argument('--date-format',
+        help = 'The format to use for timestamps.')
+
+    fuse_args = {}
     class OAction(argparse.Action):
         def __call__(self, parser, namespace, values, option_string):
             try:
                 name, value = values[0].split('=')
             except ValueError:
                 name, value = values[0], True
-            setattr(namespace, name, value)
+            fuse_args[name] = value
     parser.add_argument('-o',
         help = 'Any FUSE options.',
         nargs = 1,
         action = OAction)
 
-    # First, let args be the argument dict
-    args = vars(parser.parse_args())
 
-    # Then pop these known items and pass them on to the PhotoFS constructor
-    photofs_args = {name: args.pop(name)
-        for name in (
-            'source',
-            'photo_path',
-            'video_path',
-            'date_format')
-        if not args.get(name, None) is None}
-
-    # Then copy all non-None values and pass them to the FUSE constructor
-    fuse_args = {name: value
-        for name, value in args.items()
+    # First, let args be the argument dict, but remove undefined values
+    args = {name: value
+        for name, value in vars(parser.parse_args()).items()
         if not value is None}
 
+    # Then pop these known items and pass them on to the FUSE constructor
+    fuse_args.update({name: args.pop(name)
+        for name in (
+            'foreground',
+            'debug')
+        if name in args})
+
     try:
-        photo_fs = PhotoFS(args['mountpoint'], **photofs_args)
-        fuse.FUSE(photo_fs, fsname = 'photofs', **fuse_args)
+        photo_fs = PhotoFS(**args)
+        fuse.FUSE(photo_fs, args['mountpoint'], fsname = 'photofs', **fuse_args)
     except Exception as e:
         import traceback; traceback.print_exc()
         try:
