4e9203afe90a0e3c1b09ec9664341dcc8e0a7842
Moses Palmér <mosespalmer@gmail.com>
2016-03-05 12:05:03 +0100

    Updated copyright

--- lib/photofs/_tag.py
+++ lib/photofs/_tag.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 # photofs
-# Copyright (C) 2012-2014 Moses Palmér
+# Copyright (C) 2012-2016 Moses Palmér
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
